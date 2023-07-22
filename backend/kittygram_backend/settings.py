d93400c50d885108ea77142720e0bd1d65ef3dd7
Михаил Матросов <matrosovmn@gmail.com>
2023-07-23 03:12:45 +0500

    debug3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
-        'PORT': os.getenv('DB_PORT', ),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
